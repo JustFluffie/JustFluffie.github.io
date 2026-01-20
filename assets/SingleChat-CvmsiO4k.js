b305351a77be146d1e1648f22a3ee0963d1e2f03
JustFluffie <quellaluo1112@gmail.com>
2026-01-20 18:24:36 +0000

    Deploying to gh-pages from @ JustFluffie/JustFluffie.github.io@f5e4c7a97ac4b44ef0d936448077a4f503e9431f 🚀

--- assets/SingleChat-CvmsiO4k.js
+++ assets/SingleChat-CvmsiO4k.js
@@ -1,4 +1,4 @@
-import{t as G,H as Ze,I as _e,r as x,J as ke,L as et,a as tt,k as de,x as _,G as ae,_ as O,q as Q,y as ue,b as S,o as h,l as W,e as D,f as q,d as e,T as re,B as k,j as y,h as H,w as j,A as Y,M as ee,c as L,F,C as X,n as U,v as ce,E as Z,N as ye,i as Se,z,S as oe,O as st,D as J,P as nt,Q as ot,R as at,U as K,V as lt,W as ne,X as it}from"./index-CypFzVNd.js";const rt=(t,v)=>{const a=v.map(n=>`${n.role==="user"?"用户":t.name}: ${n.content}`).join(`
+import{t as G,H as Ze,I as _e,r as x,J as ke,L as et,a as tt,k as de,x as _,G as ae,_ as O,q as Q,y as ue,b as S,o as h,l as W,e as D,f as q,d as e,T as re,B as k,j as y,h as H,w as j,A as Y,M as ee,c as L,F,C as X,n as U,v as ce,E as Z,N as ye,i as Se,z,S as oe,O as st,D as J,P as nt,Q as ot,R as at,U as K,V as lt,W as ne,X as it}from"./index-ygyB_2sH.js";const rt=(t,v)=>{const a=v.map(n=>`${n.role==="user"?"用户":t.name}: ${n.content}`).join(`
 `);return`
 你正在扮演角色“${t.name}”，你需要根据以下设定和最近的对话，生成一段角色的实时“心声”。
 心声由多个部分组成，请严格按照下面的JSON格式返回，不要添加任何额外的解释或文字。
